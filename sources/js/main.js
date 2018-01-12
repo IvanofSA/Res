5910a9b1fe2606112dbd02730e9bdea25f0e2a80
Poshelya <poshelya@gmail.com>
2018-01-12 17:31:44 +0300

    page resume

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,4 +1,26 @@
-let menuBtn = document.querySelector('.menu');
+
+const menuBtn = document.querySelector('.js-menu');
+const doughnutData = [
+    {
+        value: 50,
+        color:"#ffce29",
+        highlight: "#ffbd23",
+        label: "HTML"
+    },
+    {
+        value: 20,
+        color: "#ea6a45",
+        highlight: "#ff593f",
+        label: "SASS"
+    },
+    {
+        value: 30,
+        color: "#5d9bd6",
+        highlight: "#247ed6",
+        label: "JavaScript"
+    }
+];
+
 
 menuBtn.addEventListener('click', function (e) {
     e.preventDefault();
@@ -9,3 +31,19 @@ menuBtn.addEventListener('click', function (e) {
         this.classList.toggle('open');
     }
 });
+
+
+window.onload = function(){
+    let ctx = document.getElementById("chart-area").getContext("2d");
+
+    let myDoughnut = new Chart(ctx).Doughnut(doughnutData, {
+        responsive : true,
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        },
+        cutoutPercentage: 80,
+    });
+
+};
+
