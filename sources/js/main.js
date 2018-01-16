9ccfd320b2889bc27825a327dfe4a9f817aa8555
Poshelya <poshelya@gmail.com>
2018-01-16 18:50:04 +0300

    works page, animate, filters

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,9 +1,22 @@
+import {animateHover} from './modules/module.animate';
 
-const menuBtn = document.querySelector('.js-menu');
+
+let menuBtn = document.querySelector('.js-menu');
+menuBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    let target = e.target;
+
+    if (target.classList.contains('burger__icon')) {
+        this.classList.toggle('open');
+    }
+});
+
+
 const doughnutData = [
     {
         value: 50,
-        color:"#ffce29",
+        color: "#ffce29",
         highlight: "#ffbd23",
         label: "HTML"
     },
@@ -20,30 +33,126 @@ const doughnutData = [
         label: "JavaScript"
     }
 ];
+let ctx = document.getElementById("chart-area").getContext("2d");
+
+let myDoughnut = new Chart(ctx).Doughnut(doughnutData, {
+    responsive: true,
+    animation: {
+        animateScale: true,
+        animateRotate: true
+    },
+    cutoutPercentage: 80,
+});
 
 
-menuBtn.addEventListener('click', function (e) {
+let itemWork = document.querySelectorAll('.js-animate');
+
+for (let i = 0; i < itemWork.length; i++) {
+    itemWork[i].addEventListener('mouseenter', function (e) {
+        e.preventDefault();
+        let itemLink = this.querySelector('.preview__link');
+
+        animateHover({
+            duration: 800,
+            timing: function (timeFraction) {
+                return timeFraction;
+            },
+            draw: function (progress) {
+                let specNum = progress * -260;
+                itemLink.style.left = -260 - specNum + 'px';
+            }
+        });
+
+    });
+
+    itemWork[i].addEventListener('mouseleave', function (e) {
+        e.preventDefault();
+        let itemLink = this.querySelector('.preview__link');
+
+        animateHover({
+            duration: 1000,
+            timing: function (timeFraction) {
+                return Math.pow(timeFraction, 2);
+            },
+            draw: function (progress) {
+                itemLink.style.left = progress * 260 + 'px';
+            }
+        });
+
+    });
+}
+
+
+let filterBtn = document.querySelector('.js-filter');
+let previewItems = document.querySelectorAll('.js-animate');
+
+filterBtn.addEventListener('click',  (e) => {
     e.preventDefault();
-
     let target = e.target;
-
-    if (target.classList.contains('burger__icon')) {
-        this.classList.toggle('open');
+    if (target.classList.contains('filter__link')) {
+        filterPreview(target, previewItems);
     }
 });
 
 
-window.onload = function(){
-    let ctx = document.getElementById("chart-area").getContext("2d");
+function filterPreview(btn, arr) {
 
-    let myDoughnut = new Chart(ctx).Doughnut(doughnutData, {
-        responsive : true,
-        animation: {
-            animateScale: true,
-            animateRotate: true
-        },
-        cutoutPercentage: 80,
+    let dataFilter = btn.getAttribute('data-filter');
+
+
+    arr.forEach((el, i, arr) => {
+
+        let itemData = el.getAttribute('data-item');
+        if(dataFilter === 'all') {
+            // animateHover({
+            //     duration: 1000,
+            //     timing: function (timeFraction) {
+            //         return Math.pow(timeFraction, 2);
+            //     },
+            //     draw: function (progress) {
+            //         el.style.opacity = progress * 1 + 'px';
+            //
+            //         // el.style.width = progress * 125 + 'px';
+            //         el.style.display = 'block';
+            //
+            //     }
+            // });
+        } else {
+            if(dataFilter === itemData) {
+                animateHover({
+                    duration: 1000,
+                    timing: function (timeFraction) {
+                        return Math.pow(timeFraction, 2);
+                    },
+                    draw: function (progress) {
+                        // el.style.width = progress * 125 + 'px';
+                        el.style.display = 'block';
+                        el.style.height = progress * 125 + 'px';
+                    }
+                })
+
+            } else {
+                console.log('no');
+                animateHover({
+                    duration: 1000,
+                    timing: function (timeFraction) {
+                        return Math.pow(timeFraction, 2);
+                    },
+                    draw: function (progress) {
+                        console.log(progress);
+                        if(progress <= 0.9) {
+                            el.style.height = '0px';
+                        } else {
+                            el.style.display = 'none';
+                        }
+                        // el.style.width = progress * 125 + 'px';
+                        // el.style.display = 'none';
+
+                    }
+                });
+            }
+        }
+
     });
 
-};
-
+}
